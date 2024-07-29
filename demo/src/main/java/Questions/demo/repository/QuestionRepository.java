6b4d1a34d8b4c9f22c3956a097594fbc026d9a88
dteran24 <dteran21@yahoo.com>
2024-07-29 12:07:28 -0500

    added query to include tags with react questions

--- demo/src/main/java/Questions/demo/repository/QuestionRepository.java
+++ demo/src/main/java/Questions/demo/repository/QuestionRepository.java
@@ -7,6 +7,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    @Query(value = "SELECT user_questions.*, question_tags.*\n" +
+            "FROM user_questions\n" +
+            "INNER JOIN question_tags ON user_questions.id = question_tags.question_id;", nativeQuery = true)
+    List<Question> getAllQuestionsWithTags();
 }
